f8c47f0ced682106c73707632889eb95c12f1cd6
Rishita-22 <rishitapatri@gmail.com>
2026-06-12 21:22:04 +0530

    Added STATIC_ROOT

--- customer_portal/settings.py
+++ customer_portal/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-b&_0^r@+*58o^5bj-i#n5pda(&e57_xn6b(8vjln9p2km^^8in
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -42,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,4 +115,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
